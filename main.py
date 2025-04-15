3f8580302fa69b06a39a5f3db08556075274376e
AbdullahGit-ui <pathana2@montclair.edu>
2025-04-15 00:34:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -236,4 +236,5 @@ def search_stocks():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    #uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    gunicorn main:app --bind 0.0.0.0:$PORT
